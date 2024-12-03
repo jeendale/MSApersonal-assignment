16e7ae0fb10821ca67c430e65e6357efeb508335
jeendale <jeekoo7203@gmail.com>
2024-12-04 02:23:13 +0900

    feat: auth server port response header 추가

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/controller/AuthController.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.sparta.msa_exam.auth.dto.AuthRequestDto;
 import com.sparta.msa_exam.auth.dto.SignInResDto;
 import com.sparta.msa_exam.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,19 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
+    @Value("${server.port}")
+    private String serverPort;
+
     private final AuthService authService;
 
     @PostMapping("/signIn")
     public ResponseEntity<AuthRequestDto> signIn(@RequestBody SignInResDto signInResDto){
         AuthRequestDto authRequestDto=authService.signIn(signInResDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Server-Port", serverPort);
 
-        return ResponseEntity.ok(authRequestDto);
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(authRequestDto);
     }
 
     @PostMapping("/signUp")
     public ResponseEntity<String> signUp(@RequestBody SignInResDto signInResDto ){
         authService.signUp(signInResDto);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Server-Port", serverPort);
 
-        return ResponseEntity.ok("회원가입 성공");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body("회원가입 성공");
     }
 }
